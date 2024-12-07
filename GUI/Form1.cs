1d19448474231089453df70979520f1d362836e2
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-12-07 07:47:40 +0200

    add class Figure with fields and constructor

---
+++ GUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+    public abstract class Figure
+    {
+        protected int x;
+        protected int y;
+
+        public Figure(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
